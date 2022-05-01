e6576c839c06dc98b1c6ef6e7f6ed11a19611863
egrillini <enrico.grillini@gmail.com>
2022-05-01 16:35:18 +0000

    Allineamento

--- src/app/page/home-page/home-page.component.ts
+++ src/app/page/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from "src/app/service/auth.service";
+import { Component, OnInit, Output } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 
 export interface PeriodicElement {
   name: string;
@@ -9,16 +9,26 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 11, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 12, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 13, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 14, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 15, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 16, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 17, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 18, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 19, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 20, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
 @Component({
@@ -27,14 +37,48 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  // MatPaginator Output
+
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  dataSource: PeriodicElement[] = [];
 
+  // MatPaginator Inputs
+  length = ELEMENT_DATA.length;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+
+  constructor() {
+    this.refreshData(0, this.pageSize);
+  }
 
   ngOnInit(): void {
   }
 
-  
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
+  }
+
+  private refreshData(pageIndex: number, pageSize: number) {
+    let i = 0;
+    let result: PeriodicElement[] = [];
+    ELEMENT_DATA.forEach(aaa => {
+      let from = (pageIndex) * pageSize;
+      let to = (pageIndex + 1) * pageSize - 1;
+
+      if (i >= from && i <= to) { result.push(aaa); }
+      i++;
+    });
+
+    this.dataSource = result;
+  }
+
+  // used to build a slice of papers relevant at any given time
+  public getPaginatorData(event: PageEvent): PageEvent {
+    this.refreshData(event.pageIndex, event.pageSize);
+    return event;
+  }
 
 }
